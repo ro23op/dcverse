0fc99c88a3dc8198f527e670209fdef152c7964a
ro23op <rohannoronha8@gmail.com>
2025-05-15 18:07:48 +0530

    final

--- client/vite.config.js
+++ client/vite.config.js
@@ -17,7 +17,8 @@ export default defineConfig(({ mode }) => {
     },
     preview: {
       host: '0.0.0.0',
-      port: env.PORT ? Number(env.PORT) : 3000
+      port: env.PORT ? Number(env.PORT) : 3000,
+      allowedHosts: ['dcverse-1.onrender.com']
     }
   }
 })
