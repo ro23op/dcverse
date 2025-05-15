9f629a835535b279e6e91ddd1b0a50b18d38e89a
ro23op <rohannoronha8@gmail.com>
2025-05-15 18:05:21 +0530

    done

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import process from 'process'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    server: {
+      host: '0.0.0.0',
+      port: env.PORT ? Number(env.PORT) : 3000,
+    },
+    preview: {
+      host: '0.0.0.0',
+      port: env.PORT ? Number(env.PORT) : 3000
+    }
+  }
 })
+
+
